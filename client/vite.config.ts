45ea79edced82a34e7feaa227f63dad943f36b57
ValdemirJunior2020 <your-infojr.83@gmail.com>
2026-06-18 10:07:45 -0400

    poihugbn

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,18 +1,22 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: "0.0.0.0",
     port: 5173,
-    strictPort: false
+    strictPort: false,
+    open: true
   },
   preview: {
+    host: "0.0.0.0",
     port: 4173,
-    strictPort: false
+    strictPort: false,
+    open: true
   },
   build: {
-    outDir: 'dist',
+    outDir: "dist",
     sourcemap: false
   }
 });
